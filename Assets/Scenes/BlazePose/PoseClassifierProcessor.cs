d6013b28c32d794e04a302734e2119503779f9ec
SonicHedghog <kylehenry7000@gmail.com>
2021-05-21 12:50:17 -0500

    Fixed Pose Class Names

--- Assets/Scenes/BlazePose/PoseClassifierProcessor.cs
+++ Assets/Scenes/BlazePose/PoseClassifierProcessor.cs
@@ -16,8 +16,8 @@ namespace TensorFlowLite
     // Specify classes for which we want rep counting.
     // These are the labels in the given {@code POSE_SAMPLES_FILE}. You can set your own class labels
     // for your pose samples.
-    private static readonly String PUSHUPS_CLASS = "pushups_down";
-    private static readonly String SQUATS_CLASS = "squats_down";
+    private static readonly String PUSHUPS_CLASS = "seated_march_left";
+    private static readonly String SQUATS_CLASS = "seated_march_right";
     private static readonly String[] POSE_CLASSES = {
       PUSHUPS_CLASS, SQUATS_CLASS
     };
