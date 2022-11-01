c0b4b020560aa4218b3c45f66e81b526d33c6e28
Sabrina Haque <sabrina.hq@gmail.com>
2022-10-31 19:28:23 -0500

    added UI for activity record

---
+++ Assets/Scenes/Scripts/General/ActivityDetails.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using Mono.Data.Sqlite;
+using System.Data;
+
+
+public class ActivityDetails : MonoBehaviour
+{
+     public string date,start_time,message="";
+    
+    public Sprite exerciseSprite;
+    public Sprite walkSprite;
+
+    //*******sqlite database
+    private SqliteConnection dbconn;
+    private string conn;
+    private SqliteCommand dbcmd;
+    private string sqlQuery;
+    string filepath;
+    private static IDataReader reader;
+    private GameObject DetailsButton;
+    private GameObject g;
+    public static string queryDate="";
+    public Button infoButton;
+    
+   // private GameObject activityContainer;
+   // public GameObject activityDetails;
+    public TextMeshProUGUI details;
+    private Dictionary<string, List<string>> dictionary =
+    new Dictionary<string,List<string>>();
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        DetailsButton=transform.GetChild(0).gameObject;
+         
+
+        
+       filepath = Application.persistentDataPath + "/SeniorFitDB.s3db";
+        conn = "URI=file:" + filepath;
+        
+       updateDetails(queryDate);
+    }
+    /*void OnEnable()
+    {
+         DetailsButton=transform.GetChild(0).gameObject;
+           
+       filepath = Application.persistentDataPath + "/SeniorFitDB.s3db";
+        conn = "URI=file:" + filepath;
+        
+     updateDetails(queryDate);
+    }*/
+
+     void updateDetails(string date)
+    {
+        
+        
+         // Save/Update contents to SQLite DB
+        using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+                    
+		//string query ="SELECT * FROM ExerciseData";
+        string query="SELECT DISTINCT Exercise FROM ExerciseData where Date=\"" +date+"\"" ;
+        Debug.Log(query);
+		dbcmd.CommandText = query;
+		reader = dbcmd.ExecuteReader();
+        Debug.Log("From database "+reader.ToString());
+
+		while (reader.Read())
+		{
+			Debug.Log("from database: " + reader[0].ToString());
+
+            g=Instantiate(DetailsButton,transform);
+
+            string eName=reader[0].ToString();
+            
+            
+            Debug.Log(eName);
+            
+
+            g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text=eName;
+            g.transform.GetComponent<Button>().onClick.AddListener(()=>detailsButtonClick(eName,date));
+
+            Debug.Log(g);
+			
+		}
+        dbconn.Close();}
+
+        //********get walk data***************//
+
+        using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+                    
+
+        string query1="SELECT * FROM WalkData where Date=\"" +date+"\"";
+        
+		dbcmd.CommandText = query1;
+		reader = dbcmd.ExecuteReader();
+       
+
+		if (reader.Read())
+		{
+			Debug.Log("from walking database: " + reader[0].ToString());
+
+            g=Instantiate(DetailsButton,transform);
+
+            string eName="Outdoor Walking";
+            
+            
+
+            g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text=eName;
+            g.transform.GetComponent<Button>().onClick.AddListener(()=>detailsButtonClick(eName,date));
+
+            Debug.Log(g);
+			
+		}
+
+		// Close connection
+		dbconn.Close();
+
+
+                }
+                Destroy(DetailsButton);
+    }
+
+    public void setData(string date){
+
+        
+        queryDate=date;
+        Debug.Log("date set:"+queryDate);
+         
+
+
+    }
+
+    public void detailsButtonClick(string name,string date)
+    {
+        string detail="";
+        Debug.Log("details for "+name+" "+date);
+        if(name!="Outdoor Walking")
+        {
+            
+
+        infoButton.GetComponent<Image>().overrideSprite = exerciseSprite;
+
+         using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+                    
+		//string query ="SELECT * FROM ExerciseData";
+        string query="SELECT * FROM ExerciseData where Exercise=\"" +name+"\" and Date=\"" +date+"\"" ;
+        Debug.Log(query);
+		dbcmd.CommandText = query;
+		reader = dbcmd.ExecuteReader();
+        Debug.Log("From database "+reader.ToString());
+
+		while (reader.Read())
+		{
+			Debug.Log("from database: " + reader[0].ToString());
+
+            detail+="Start time: "+reader[0].ToString()+"\n"+"Duration: "+reader[3].ToString()+"\n"+"Repetitions: "+reader[4].ToString()+"\n\n\n";
+            
+            details.text=detail;
+            Debug.Log(detail);
+			
+		}
+
+		// Close connection
+		dbconn.Close();
+
+
+         }
+
+        }
+        else
+        {
+            infoButton.GetComponent<Image>().overrideSprite = walkSprite;
+             using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+                    
+		//string query ="SELECT * FROM ExerciseData";
+        string query="SELECT * FROM WalkData where Date=\"" +date+"\"" ;
+        Debug.Log(query);
+		dbcmd.CommandText = query;
+		reader = dbcmd.ExecuteReader();
+        Debug.Log("From database "+reader.ToString());
+
+		while (reader.Read())
+		{
+			Debug.Log("from walking database: count");
+
+            detail+="Start time: "+reader[0].ToString()+"\n"+"End Time: "+reader[2].ToString()+"\n"+"Miles Walked: "+reader[3].ToString()+"\n\n\n";
+            
+            details.text=detail;
+            Debug.Log(g);
+			
+		}
+
+		// Close connection
+		dbconn.Close();
+
+
+         }
+
+        }
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/General/ActivityRecord.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using Mono.Data.Sqlite;
+using System.Data;
+
+public class ActivityRecord : MonoBehaviour
+{
+   
+    public string date,start_time,message="";
+    
+    public Sprite exerciseSprite;
+
+    //*******sqlite database
+     private SqliteConnection dbconn;
+    private string conn;
+    private SqliteCommand dbcmd;
+    private string sqlQuery;
+    string filepath;
+    private static IDataReader reader;
+    private GameObject activityButton;
+    private GameObject g;
+    
+   // private GameObject activityContainer;
+   // public GameObject activityDetails;
+    public TextMeshProUGUI exerciseName;
+    private Dictionary<string, List<string>> dictionary =
+    new Dictionary<string,List<string>>();
+   // private string queryDate;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+         activityButton=transform.GetChild(0).gameObject;
+         
+
+        filepath = Application.persistentDataPath + "/SeniorFitDB.s3db";
+        conn = "URI=file:" + filepath;
+
+        updateRecord();
+
+        
+    }
+
+    void updateRecord()
+    {
+        List<string>unique_dates=new List<string>();
+         // Save/Update contents to SQLite DB
+        using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+        string query="SELECT DISTINCT Date FROM (SELECT Date FROM ExerciseData UNION SELECT Date FROM WalkData)";
+        
+		//string query ="SELECT DISTINCT Date FROM ExerciseData";
+		dbcmd.CommandText = query;
+		reader = dbcmd.ExecuteReader();
+        Debug.Log("From database "+reader.ToString());
+
+
+
+		while (reader.Read())
+		{
+            unique_dates.Add(reader[0].ToString());
+				
+		}
+
+		// Close connection
+		dbconn.Close();
+
+                }
+
+        foreach (var item in unique_dates)
+            {
+                int exerciseCount=0,walkcount=0;
+                using (dbconn = new SqliteConnection(conn))
+                {
+                    dbconn.Open(); //Open connection to the database.
+                    dbcmd = dbconn.CreateCommand();
+                    string query="SELECT COUNT(DISTINCT Exercise) FROM ExerciseData WHERE Date=\"" +item+"\"";                         
+		                //string query ="SELECT DISTINCT Date FROM ExerciseData";
+		            dbcmd.CommandText = query;
+		            reader = dbcmd.ExecuteReader();
+                    exerciseCount=Int16.Parse(reader[0].ToString());
+                    Debug.Log("From database exercise count"+reader[0].ToString());
+                    dbconn.Close();
+                }
+
+                using (dbconn = new SqliteConnection(conn))
+                    {
+                        dbconn.Open(); //Open connection to the database.
+                        dbcmd = dbconn.CreateCommand();
+
+                        string query2="SELECT * FROM WalkData WHERE Date=\"" +item+"\""; 
+                        dbcmd.CommandText = query2;
+		                reader = dbcmd.ExecuteReader();
+                        if(reader.Read())
+                        {
+                             walkcount=1;
+                        }
+                       
+                        Debug.Log("From database walk count"+walkcount);
+                       
+                        dbconn.Close();
+                    }
+                string summary="";
+
+                if(exerciseCount!=0 && walkcount!=0)
+                    {
+                        summary="\nexercise: "+exerciseCount.ToString()+"\nOutdoor Walking";
+
+                    }
+                else{
+                    if(walkcount==0)
+                        summary="\nexercise: "+exerciseCount.ToString();
+
+                    if(exerciseCount==0)
+                        summary="\nOutdoor Walking";
+
+                    }
+
+     
+
+                            g=Instantiate(activityButton,transform);
+
+                            //string date_time=reader[0].ToString();
+                            System.DateTime dateTime = System.DateTime.Parse(item);
+            
+                            date=dateTime.ToString("dddd, MMMM d, yyyy");
+                            
+                            string queryDate=item;
+                            Debug.Log("distinct date "+queryDate);
+                            g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text=date+summary;
+                            //g.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text=message;
+                            g.transform.GetComponent<Button>().onClick.AddListener(()=>activityButtonClick(queryDate));
+
+                            Debug.Log(g);
+			
+		                
+            }
+        
+        
+        Destroy(activityButton);
+    }
+
+    void activityButtonClick(string date)
+    {
+        //ActivityDetails activityDetails=new ActivityDetails();
+        ActivityDetails.queryDate=date;
+        //activityDetails.setData(date);
+        
+        Debug.Log("button clicked "+date);
+        //Debug.Log(activityContainer);
+        //exerciseName.text=dictionary[date][0];
+        //activityContainer.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text=dictionary[date][0];}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
